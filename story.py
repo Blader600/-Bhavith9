69c6a4c27d238aa1b520213cbbeebcac9b171c1b
Blader600 <bhavith.pradeep@gmail.com>
2024-12-26 16:48:45 +0530

    Add files via upload

---
+++ story.py
@@ -0,0 +1,7 @@
+import random
+name_hero=["Albert is from ","Benito is from ","Charls is from ","Danniel is from "]
+name_sideman=["Chandler is from ","Jimmy is from ","Nathanis from ","Preston is from "]
+country_hero=["USA","Barcelona","England","UK"]
+state_sideman=["Calafornia","Texes","Dalas"]
+happened=["they went to a lion's den","they were stuck in a jungle"]
+print(random.choice(name_hero)+random.choice(name_sideman)+random.choice(country_hero)+random.choice(state_sideman)+random.choice(happened))
